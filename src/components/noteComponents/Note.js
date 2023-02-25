e2600420110516c161d399b77affb8074c193acd
Austin Pedigo <pedigo.was.here@gmail.com>
2023-02-25 14:44:36 -0500

    ao

---
+++ src/components/noteComponents/Note.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import { faTrash} from '@fortawesome/free-solid-svg-icons'
+function Note({id,text,deleteNote}) {
+    return(
+        <div className='note'>
+            <div className="note__body">{text}</div>
+            <div className="note__footer" style={{justifyContent: 'flex-end'}}>
+         <FontAwesomeIcon icon={faTrash} className='note__delete' onClick={() => deleteNote(id)}></FontAwesomeIcon>
+
+            </div>
+        </div>
+    )
+}
+export default Note
